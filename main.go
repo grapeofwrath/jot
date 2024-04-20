bf2f7b5e74ee03a31cac812cb008a42f41d4fdb5
grapeofwrath <69535018+grapeofwrath@users.noreply.github.com>
2024-04-20 14:18:21 -0500

    switched log for charmbraclet/log, removed flags, changed module name to notes, fixed default behavior

--- main.go
+++ main.go
@@ -1,39 +1,51 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "log"
     "os"
     "os/exec"
+    "strings"
     "time"
+
+   "github.com/charmbracelet/log"
 )
 
 func main() {
-    homeDir, err := os.UserHomeDir()
+    var homeDir, err = os.UserHomeDir()
     if err != nil {log.Fatal(err)}
+    var notesDir = homeDir+"/notes"
+    var editor = "nvim"
+    var date = time.Now().Format("01/02/2006 3:04 PM")
+    var template = "---\ntitle:\ndate: "+date+"\ntags:\n---\n\n#"
 
-    newCmd := flag.NewFlagSet("new", flag.ExitOnError)
-    notesDir := newCmd.String("dir", homeDir+"/notes", "Notes directory")
-    editor := newCmd.String("editor", "nvim", "Editor application")
-    switch os.Args[1] {
-    case "new":
-        newCmd.Parse(os.Args[2:])
+    if len(os.Args) < 2 {
+        var openEditor = exec.Command(editor)
+        openEditor.Dir = notesDir
+        openEditor.Stdin = os.Stdin
+        openEditor.Stdout = os.Stdout
+        openEditor.Stderr = os.Stderr
 
-        notePath := *notesDir+"/"+os.Args[2]+".md"
-        date := time.Now()
-        template := "---\n"+date.String()+"\n---\n\n# "+os.Args[2]
-        edit := exec.Command(*editor, notePath)
-        edit.Stdin = os.Stdin
-        edit.Stdout = os.Stdout
-        edit.Stderr = os.Stderr
-
-        err := os.WriteFile(notePath, []byte(template), 0666)
+        err = openEditor.Run()
         if err != nil {log.Fatal(err)}
-        fmt.Println("Created note: "+notePath)
-        err = edit.Run()
+    } else {
+        var fileName = strings.Join(os.Args[1:], "_")
+        var notePath = notesDir+"/"+fileName+".md"
+
+        if _, err := os.Stat(notePath); err == nil {
+            log.Fatal("Note exists: "+notePath)
+            // print files that contain fileName
+            // prompt to rename note
+            // switch log.Fatal to log.Error
+        }
+
+        var openNote = exec.Command(editor, notePath)
+        openNote.Stdin = os.Stdin
+        openNote.Stdout = os.Stdout
+        openNote.Stderr = os.Stderr
+
+        err = os.WriteFile(notePath, []byte(template), 0666)
         if err != nil {log.Fatal(err)}
-    default:
-        fmt.Println("Expected 'new' command")
+        log.Info("Creating note... "+notePath)
+        err = openNote.Run()
+        if err != nil {log.Fatal(err)}
     }
 }
